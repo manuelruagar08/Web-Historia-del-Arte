2871757167ece5b0538de92650ebf4724bb4d050
manuelruagar08 <manuel.ruagar08@gmail.com>
2026-01-22 12:38:54 +0100

    Add files via upload

---
+++ nuevo.js
@@ -0,0 +1,9 @@
+alert("Esta es mi pagina web");
+var x= 14;
+var y = 3;
+var x = parseInt(prompt("Dime un número"));
+var y = parseInt(prompt("Dime otro"));
+var suma = x + y;
+var resultado = ("La suma de los dos número es " + suma);
+document.write(resultado);
+
